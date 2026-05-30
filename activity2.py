b3757057f0fecf4c416d438897ca294debb918a2
sumandevi14456-alt <sumandevi14456@gmail.com>
2026-05-30 17:38:52 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,4 +1,24 @@
-import keyword
-print(keyword.kwlist)
-name=input("enter your name")
-print(name)
+import turtle
+
+board = turtle.Turtle()
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
